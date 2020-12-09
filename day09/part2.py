bbcd89dea5926521c9679a861059f7334ebd25c5
Vitalii Karniushin <vitalii.karniushin@cloudsolutions.co.uk>
2020-12-10 00:29:28 +0100

    Day 9: part2 improvement

--- day09/part2.py
+++ day09/part2.py
@@ -2,14 +2,14 @@ import argparse
 
 import pytest
 from support import timing
-import itertools
+from itertools import combinations
 
 
 def get_invalid(numbers, preamble):
-    chunks = [numbers[x:x+preamble] for x in range(0, len(numbers) - preamble)]
+    chunks = (numbers[x:x+preamble] for x in range(0, len(numbers) - preamble))
     i = 0
     for chunk in chunks:
-        pairs = list(itertools.combinations(chunk, 2))
+        pairs = list(combinations(chunk, 2))
         target = numbers[preamble + i]
         if any(sum(p) == target for p in pairs) is not True:
             return target
@@ -20,14 +20,13 @@ def get_invalid(numbers, preamble):
 def compute(s):
     numbers = [int(n) for n in s.splitlines()]
     invalid = get_invalid(numbers, 25)
-    max_seq = 0
-    target = ()
-    for i in range(0, numbers.index(invalid)):
-        for j in range(i + 1, numbers.index(invalid)):
-            if sum(numbers[i:j]) == invalid and j - i > max_seq:
-                max_seq = j - i
-                target = (i, j)
-    r = numbers[target[0]:target[1]]
+    (i, j, r) = (0, 1, [])
+    while sum(r) != invalid:
+        r = numbers[i:j]
+        if sum(r) < invalid:
+            j += 1
+        elif sum(r) > invalid:
+            i += 1
     return min(r) + max(r)
 
 
